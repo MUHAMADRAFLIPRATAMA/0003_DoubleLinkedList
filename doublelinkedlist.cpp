6f9d694dc27d3b9cf77ace9c5d70301d08c53c35
MUHAMADRAFLIPRATAMA <kipxrap@gmail.com>
2025-05-15 10:59:09 +0700

    struktur simpul ( node)

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -1,3 +1,14 @@
 #include <iostream>
 #include <string>
-using namespace
+using namespace std;
+ 
+class Node
+{
+        public :
+        int noMhs;
+        Node *next;
+        Node *prev;
+};
+
+
+
