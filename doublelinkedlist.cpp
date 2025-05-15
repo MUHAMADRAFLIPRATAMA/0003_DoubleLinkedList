e808b035be0428207ffdca15f32d9a0e95c6a1fe
MUHAMADRAFLIPRATAMA <kipxrap@gmail.com>
2025-05-15 12:31:05 +0700

    repeat untill currentNode == null

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -137,4 +137,18 @@ void traverse()
  //step 1: Mark first node as currentNode
  Node *currentNode = START;
 
- 
+ //step 2 : repeat until currentNode == NULL
+ cout << "\nRecords in ascending order of roll number are:\n";
+ int i = 0;
+ while (currentNode != NULL)
+ {
+     cout << i + 1 << "." << currentNode->noMhs << " " << endl;
+
+     //step 3 : Move to next node
+     currentNode = currentNode->next;
+     i++;
+ }
+
+
+}
+
