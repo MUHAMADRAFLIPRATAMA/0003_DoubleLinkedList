942ca6c0bf2bf9ae44c0528d60f179ec6a69d0d5
MUHAMADRAFLIPRATAMA <kipxrap@gmail.com>
2025-05-15 12:22:57 +0700

    delete the node

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -121,4 +121,8 @@ void hapus()
          current->next->prev = current->prev;
  }
  
- 
+ //step 5 : delete the node
+ delete current;
+ cout << "Record with roll number" << rollNo << "deleted" << endl;
+}
+
